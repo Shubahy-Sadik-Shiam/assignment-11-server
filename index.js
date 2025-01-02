425ace0e5f0184188332092fc9920fa13f7b9e2c
ShorgoShiam <shorgoshiam@gmail.com>
2025-01-02 13:37:02 +0600

    get books by category

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -18,24 +18,39 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-
     const bookCollection = client.db("bookDB").collection("books");
 
-    app.post("/allBooks", async(req, res)=>{
-        const books = req.body;
-        const result = await bookCollection.insertOne(books);
-        res.send(result);
-    })
+    app.post("/allBooks", async (req, res) => {
+      const books = req.body;
+      const result = await bookCollection.insertOne(books);
+      res.send(result);
+    });
+
+    app.get("/allBooks", async (req, res) => {
+      const cursor = bookCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/books/:category", async (req, res) => {
+      const category = req.params.category;
+      const query = {category: category};
+      const cursor = bookCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -44,9 +59,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-    res.send("Books are ready!!!");
-  });
+  res.send("Books are ready!!!");
+});
 
 app.listen(port, () => {
-    console.log("Server running successful");
-  });
+  console.log("Server running successful");
+});
