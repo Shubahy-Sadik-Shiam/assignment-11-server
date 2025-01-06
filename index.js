6561d7f4d09bc853f097982bb7c1b4adef4c93e3
ShorgoShiam <shorgoshiam@gmail.com>
2025-01-06 11:33:05 +0600

    set cookie after login

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const jwt = require('jsonwebtoken');
+const cookieParser = require("cookie-parser")
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
+app.use(cookieParser());
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173"],
+  credentials: true
+}));
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.czfhh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -27,6 +33,21 @@ async function run() {
     const borrowedBookCollection = client
       .db("bookDB")
       .collection("borrowedBooks");
+
+      // JWT authentication related apis----->
+
+      app.post("/jwt", (req, res)=>{
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+          expiresIn: "5h"
+        })
+        res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false
+        })
+        .send({success: true});
+      })
 
     app.post("/allBooks", async (req, res) => {
       const books = req.body;
